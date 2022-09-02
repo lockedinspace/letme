40b95f06de4f5e27ec11f0f41659c9b1c97ac871
tim <tim@workforce.locked>
2022-09-02 16:01:57 +0200

    formatted output

--- pkg/cmd/config-file.go
+++ pkg/cmd/config-file.go
@@ -1,71 +1,71 @@
 package letme
 
 import (
-        "bufio"
-        "fmt"
-        "github.com/lockedinspace/letme-go/pkg"
-        "github.com/spf13/cobra"
-        "os"
-        "path/filepath"
+	"bufio"
+	"fmt"
+	"github.com/lockedinspace/letme-go/pkg"
+	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
 )
 
 var configFileCmd = &cobra.Command{
-        Use:   "config-file",
-        Short: "Create a config file needed by letme where parameters such as MFA arn are stored.",
-        Long: `Use the command 'config-file' to create a toml template with all the key-value pairs needed by letme.
+	Use:   "config-file",
+	Short: "Create a config file needed by letme where parameters such as MFA arn are stored.",
+	Long: `Use the command 'config-file' to create a toml template with all the key-value pairs needed by letme.
 The config file is created on '$HOME/.letme/letme-config', letme needs this file
 to perform aws calls. Once created, you will need to edit that file and fill it with 
 your values.
         `,
-        Run: func(cmd *cobra.Command, args []string) {
-                // grab and define force flag
-                forceFlag, _ := cmd.Flags().GetBool("force")
+	Run: func(cmd *cobra.Command, args []string) {
+		// grab and define force flag
+		forceFlag, _ := cmd.Flags().GetBool("force")
 
-                // define file name and grab user home directory
-                fileName := "letme-config"
-                homeDir := utils.GetHomeDirectory()
+		// define file name and grab user home directory
+		fileName := "letme-config"
+		homeDir := utils.GetHomeDirectory()
 
-                // conditional statement which creats either the directory + config file or just the config file if the directory already exists
-                // then writes  the marshalled values on a toml document (letme-config).
-                if _, err := os.Stat(homeDir + "/.letme/"); err != nil {
-                        err = os.Mkdir(homeDir+"/.letme/", 0700)
-                        utils.CheckAndReturnError(err)
+		// conditional statement which creats either the directory + config file or just the config file if the directory already exists
+		// then writes  the marshalled values on a toml document (letme-config).
+		if _, err := os.Stat(homeDir + "/.letme/"); err != nil {
+			err = os.Mkdir(homeDir+"/.letme/", 0700)
+			utils.CheckAndReturnError(err)
 
-                        configFile, err := os.Create(filepath.Join(homeDir+"/.letme/", filepath.Base(fileName)))
-                        utils.CheckAndReturnError(err)
-                        defer configFile.Close()
+			configFile, err := os.Create(filepath.Join(homeDir+"/.letme/", filepath.Base(fileName)))
+			utils.CheckAndReturnError(err)
+			defer configFile.Close()
 
-                        writer := bufio.NewWriter(configFile)
-                        _, err = fmt.Fprintf(writer, "%v", utils.TemplateConfigFile())
-                        utils.CheckAndReturnError(err)
-                        writer.Flush()
-                        fmt.Println("letme: edit the config file at " + homeDir + "/.letme/letme-config with your values.")
-                } else if _, err := os.Stat(homeDir + "/.letme/"); err == nil {
-                        if _, err = os.Stat(homeDir + "/.letme/" + fileName); err == nil && !(forceFlag) {
-                                fmt.Println("letme: letme-config file already exists at: " + homeDir + "/.letme/" + fileName)
-                                fmt.Println("letme: to restore the letme-config file, pass the -f, --force flags or delete the letme-config file manually.")
-                                os.Exit(0)
-                        }
-                        configFile, err := os.Create(filepath.Join(homeDir+"/.letme/", filepath.Base(fileName)))
-                        utils.CheckAndReturnError(err)
-                        defer configFile.Close()
+			writer := bufio.NewWriter(configFile)
+			_, err = fmt.Fprintf(writer, "%v", utils.TemplateConfigFile())
+			utils.CheckAndReturnError(err)
+			writer.Flush()
+			fmt.Println("letme: edit the config file at " + homeDir + "/.letme/letme-config with your values.")
+		} else if _, err := os.Stat(homeDir + "/.letme/"); err == nil {
+			if _, err = os.Stat(homeDir + "/.letme/" + fileName); err == nil && !(forceFlag) {
+				fmt.Println("letme: letme-config file already exists at: " + homeDir + "/.letme/" + fileName)
+				fmt.Println("letme: to restore the letme-config file, pass the -f, --force flags or delete the letme-config file manually.")
+				os.Exit(0)
+			}
+			configFile, err := os.Create(filepath.Join(homeDir+"/.letme/", filepath.Base(fileName)))
+			utils.CheckAndReturnError(err)
+			defer configFile.Close()
 
-                        writer := bufio.NewWriter(configFile)
-                        _, err = fmt.Fprintf(writer, "%v", utils.TemplateConfigFile())
-                        utils.CheckAndReturnError(err)
-                        writer.Flush()
-                        fmt.Println("letme: edit the config file at " + homeDir + "/.letme/letme-config with your values.")
-                }
-        },
+			writer := bufio.NewWriter(configFile)
+			_, err = fmt.Fprintf(writer, "%v", utils.TemplateConfigFile())
+			utils.CheckAndReturnError(err)
+			writer.Flush()
+			fmt.Println("letme: edit the config file at " + homeDir + "/.letme/letme-config with your values.")
+		}
+	},
 }
 
 func init() {
-        // define a Region boolean variable
-        var Region bool
+	// define a Region boolean variable
+	var Region bool
 
-        rootCmd.AddCommand(configFileCmd)
+	rootCmd.AddCommand(configFileCmd)
 
-        // create a local force flag
-        configFileCmd.Flags().BoolVarP(&Region, "force", "f", false, "bypass safety restrictions and force a command to be run")
-        configFileCmd.Flags().BoolVarP(&Region, "verify", "v", false, "verify config file structure and integrity")
+	// create a local force flag
+	configFileCmd.Flags().BoolVarP(&Region, "force", "f", false, "bypass safety restrictions and force a command to be run")
+	configFileCmd.Flags().BoolVarP(&Region, "verify", "v", false, "verify config file structure and integrity")
 }
--- pkg/utils.go
+++ pkg/utils.go
@@ -1,76 +1,76 @@
 package utils
 
 import (
-        "os"
-        "os/exec"
-        "fmt"
-        "bytes"
-        "reflect"
-        "github.com/BurntSushi/toml"
+	"bytes"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"os"
+	"os/exec"
+	"reflect"
 )
+
 // struct to unmarshall toml (wiill be modified as new options are demanded)
 type GeneralParams struct {
-        Aws_source_profile        string
-        Aws_source_profile_region string `toml:"aws_source_profile_region,omitempty"`
-        Dynamodb_table            string
-        Mfa_arn                   string `toml:"mfa_arn,omitempty"`
+	Aws_source_profile        string
+	Aws_source_profile_region string `toml:"aws_source_profile_region,omitempty"`
+	Dynamodb_table            string
+	Mfa_arn                   string `toml:"mfa_arn,omitempty"`
 }
-
 
 // this function checks if a command exists
 func CommandExists(command string) {
-        _, err := exec.LookPath(command)
-        CheckAndReturnError(err)
+	_, err := exec.LookPath(command)
+	CheckAndReturnError(err)
 }
 
 // this function checks the error, if the error contains a message, stop the execution and show the error to the user
 func CheckAndReturnError(err error) {
-        if err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-        }
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 // marshall data into a toml file
 func TemplateConfigFile() string {
-        var (
-                buf = new(bytes.Buffer)
-        )
-        err := toml.NewEncoder(buf).Encode(map[string]interface{}{
-                "general": map[string]string{
-                        "aws_source_profile":        "",
-                        "aws_source_profile_region": "",
-                        "dynamodb_table":            "",
-                        "mfa_arn":                   "",
-                },
-        })
-        CheckAndReturnError(err)
-        return buf.String()
+	var (
+		buf = new(bytes.Buffer)
+	)
+	err := toml.NewEncoder(buf).Encode(map[string]interface{}{
+		"general": map[string]string{
+			"aws_source_profile":        "",
+			"aws_source_profile_region": "",
+			"dynamodb_table":            "",
+			"mfa_arn":                   "",
+		},
+	})
+	CheckAndReturnError(err)
+	return buf.String()
 }
 
 // this function returns the caller $HOME directory
 func GetHomeDirectory() string {
-        homeDir, err := os.UserHomeDir()
-        CheckAndReturnError(err)
-        return homeDir
+	homeDir, err := os.UserHomeDir()
+	CheckAndReturnError(err)
+	return homeDir
 }
 
-// this function parsers the struct and returns one field (string only) at a time 
+// this function parsers the struct and returns one field (string only) at a time
 func ConfigFileResultString(field string) string {
-        type structUnmarshall = GeneralParams
+	type structUnmarshall = GeneralParams
 	type general map[string]structUnmarshall
-        var generalConfig general
-        _, err := toml.DecodeFile(GetHomeDirectory() + "/.letme/letme-config", &generalConfig)
-        CheckAndReturnError(err)
-        var exportedField string
-        for _, name := range []string{"general"} {
-                a := generalConfig[name]
-                r := reflect.ValueOf(a)
-                f := reflect.Indirect(r).FieldByName(field)
-                exportedField = string(f.String())
-                
-        }
-        return exportedField
-}       
+	var generalConfig general
+	_, err := toml.DecodeFile(GetHomeDirectory()+"/.letme/letme-config", &generalConfig)
+	CheckAndReturnError(err)
+	var exportedField string
+	for _, name := range []string{"general"} {
+		a := generalConfig[name]
+		r := reflect.ValueOf(a)
+		f := reflect.Indirect(r).FieldByName(field)
+		exportedField = string(f.String())
+
+	}
+	return exportedField
+}
 
 // TODO: function which validates a toml file against a struct

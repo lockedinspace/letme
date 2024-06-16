b4672be744a825a484f2488366464364fba959b8
Héctor Ruiz <ruac.hector@gmail.com>
2024-06-16 23:43:34 +0200

    patch: check renew flag on cached

--- pkg/utils.go
+++ pkg/utils.go
@@ -603,7 +603,7 @@ func GetContextData(context string) *LetmeContext {
 
 func AssumeRole(letmeContext *LetmeContext, cfg aws.Config, inlineTokenMfa string, account *DynamoDbAccountConfig, renew bool, localCredentialProcessFlagV1 bool, authMethod string) (ProfileCredential, ProfileConfig) {
 	// If credentials not expired
-	if CheckAccountAvailability(account.Name) && !localCredentialProcessFlagV1 {
+	if CheckAccountAvailability(account.Name) && !localCredentialProcessFlagV1 && !renew {
 		cachedCredentials := ReturnAccountCredentials(account.Name)
 		profileCredential := ProfileCredential{
 			AccessKey:    cachedCredentials["AccessKeyId"],
@@ -677,7 +677,7 @@ func AssumeRole(letmeContext *LetmeContext, cfg aws.Config, inlineTokenMfa strin
 
 func AssumeRoleChained(letmeContext *LetmeContext, cfg aws.Config, inlineTokenMfa string, account *DynamoDbAccountConfig, renew bool, localCredentialProcessFlagV1 bool, authMethod string) (ProfileCredential, ProfileConfig) {
 	// If credentials not expired
-	if CheckAccountAvailability(account.Name) && !localCredentialProcessFlagV1 {
+	if CheckAccountAvailability(account.Name) && !localCredentialProcessFlagV1 && !renew{
 		cachedCredentials := ReturnAccountCredentials(account.Name)
 		profileCredential := ProfileCredential{
 			AccessKey:    cachedCredentials["AccessKeyId"],

153fc66b36432f7fbe012db422676513e4f35774
gonzalo <gonzalo.vidal@nubersia.com>
2025-02-17 00:14:05 +0100

    remove debug purposes

--- pkg/cmd/config/new-context.go
+++ pkg/cmd/config/new-context.go
@@ -12,7 +12,6 @@ import (
 var NewContext = &cobra.Command{
 	Use: "new-context",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		fmt.Println("debug purposes")
 		utils.LetmeConfigCreate()
 		utils.ConfigFileHealth()
 	},

712bf44c6810c57c0211f996e02e91e4fa31d493
gonzalo <gonzalo.vidal@nubersia.com>
2025-02-20 22:32:11 +0100

    adding --context flag to config validate

--- pkg/cmd/config/validate-config-file.go
+++ pkg/cmd/config/validate-config-file.go
@@ -14,18 +14,21 @@ var Validate = &cobra.Command{
 		utils.LetmeConfigCreate()
 		utils.ConfigFileHealth()
 	},
-	Short: "Validate or create the config file.",
-	Long:  `Validate or create the config file. Check also for its file structure integrity.`,
+	Short: "Validates the config file structure and context parameters.",
+	Long:  `Validates if the config file has the right structure. Pass the --context flag if you want to validate the context endpoint reachability.`,
 	Args:  cobra.ExactArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
+		context, err := cmd.Flags().GetString("context")
 		homeDir := utils.GetHomeDirectory()
-
 		if _, err := os.Stat(homeDir + "/.letme/" + "letme-config"); err == nil {
 			result := utils.CheckConfigFile(utils.GetHomeDirectory() + "/.letme/letme-config")
 			if !result {
 				utils.TemplateConfigFile(true)
 			}
-			fmt.Println("letme: config file valid.")
+			fmt.Println("letme: the letme config file structure is valid.")
+			if context {
+				fmt.Print("context endpoint validation called")
+			}
 			os.Exit(0)
 		} else {
 			utils.CheckAndReturnError(err)
@@ -35,4 +38,6 @@ var Validate = &cobra.Command{
 
 func init() {
 	ConfigCmd.AddCommand(Validate)
+	ConfigCmd.Flags().StringP("context", "", "", "validates the context endpoint's reachability")
+
 }
